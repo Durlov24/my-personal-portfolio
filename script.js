aa12a5e632fd9b6bd3c1d61c3203296cea65debf
Durlov24 <maksudulamindurlov@gmail.com>
2024-03-19 23:16:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* // Submit form data to Google Sheets
+$('#contact-form').submit(function (e) {
+    e.preventDefault();
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+    // Code to send form data to Google Sheets
+    // This requires setting up a server-side script to handle form submissions
+});
+
+// Download CV
+$('#download-cv').click(function () {
+    // Code to trigger CV download
+}); */
